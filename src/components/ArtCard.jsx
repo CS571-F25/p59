db56bf512b6b0d62cfde427498fec4bf05f32dc8
Alana Crisostomo <acrisostomo@wisc.edu>
2025-11-18 21:03:02 -0600

    fixed image src path

--- src/components/ArtCard.jsx
+++ src/components/ArtCard.jsx
@@ -7,7 +7,7 @@ import '@/css/Gallery.css';
 function ArtCard() {
   return <>
     <div className="art-card">
-      <img src="public//images/Rooster.jpg" alt="Art Placeholder" />
+      <img src="/p59/images/Rooster.jpg" alt="Art Placeholder" />
       <h3>Artwork Title</h3>
     </div>
   </>
